77a72fd6415cce925595ca9b171cc2d3ee5eb1cc
emon4075 <mdabdullahemon4075@gmail.com>
2023-09-22 20:03:06 +0600

    22/9/2023

---
+++ EvenNumbers.cpp
@@ -0,0 +1,23 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/B
+#include <iostream>
+using namespace std;
+int main()
+{
+    int N;
+    cin >> N;
+    if (N > 1)
+    {
+        for (int i = 1; i <= N; i++)
+        {
+            if (i % 2 == 0)
+            {
+                cout << i << endl;
+            }
+        }
+    }
+    else
+    {
+        cout << -1 << endl;
+    }
+    return 0;
+}
