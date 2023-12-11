0177b566efbd6c8857f2b8d3f1673895608d5cf5
codedevastator <codedevastator@gmail.com>
2023-12-11 23:27:07 +0600

    First CodeForce Submission From Linux

---
+++ A_Magical_Sticks.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int T;
+  cin >> T;
+  while (T--) {
+    int N;
+    cin>>N;
+    int Result=ceil(N/2.0);
+    cout<<Result<<endl;
+  }
+  return 0;
+}
